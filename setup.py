1b992ac726748252f094a0680bbf97584366a09e
Hagai Kariti <hkariti@gmail.com>
2021-08-21 13:35:46 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='bitwarden',
+      version='0.1',
+      py_modules=['foo'],
+      description='Python wrapper for BitWarden CLI',
+      author='Hagai Kariti',
+      author_email='hkariti@gmail.com',
+      packages=['bitwarden'],
+      )
